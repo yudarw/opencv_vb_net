a1d2b6bfbeb7197451cc41de08f07ce384965bb4
ARLAB <ARLAB@DESKTOP-E0U4RTC>
2022-07-06 20:42:33 +0800

    Add project files.

---
+++ opencv_vb/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ImageBox1 = New Emgu.CV.UI.ImageBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.ImageBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(256, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "load Image"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ImageBox1
+        '
+        Me.ImageBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ImageBox1.Name = "ImageBox1"
+        Me.ImageBox1.Size = New System.Drawing.Size(362, 251)
+        Me.ImageBox1.TabIndex = 2
+        Me.ImageBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 274)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Number of shape detected:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 274)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 302)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ImageBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.ImageBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ImageBox1 As Emgu.CV.UI.ImageBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ opencv_vb/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.Util
+Imports System.Windows.Forms
+Imports System.Drawing
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inputImg As New Mat
+        Dim grayImg As New Mat
+        Dim binaryImg As New Mat
+        Dim contours As New Emgu.CV.Util.VectorOfVectorOfPoint()
+        Dim hier As New Mat()
+
+        'inputImg = CvInvoke.Imread("F:/image/square.jpg")
+        inputImg = CvInvoke.Imread("F:/image/Untitled.png")
+
+        'Convert to gray
+        CvInvoke.CvtColor(inputImg, grayImg, ColorConversion.Bgr2Gray)
+
+        'Apply contour detection
+        CvInvoke.Threshold(grayImg, binaryImg, 150, 255, ThresholdType.Binary)
+
+        'Detect contours
+        CvInvoke.FindContours(binaryImg, contours, hier, RetrType.Tree, ChainApproxMethod.ChainApproxSimple)
+        CvInvoke.DrawContours(inputImg, contours, -1, New MCvScalar(0, 255, 0), 5)
+
+        ' Detect the number of object
+        Dim numberobj As Integer
+        numberobj = contours.Size
+
+        If (numberobj > 0) Then
+            Label1.Text = "Object Detected"
+            Label2.Text = numberobj.ToString()
+        Else
+            Label1.Text = "No Object Detected"
+            Label2.Text = "0"
+        End If
+
+        ImageBox1.SizeMode = PictureBoxSizeMode.Zoom
+        ImageBox1.Image = inputImg
+
+    End Sub
+End Class
